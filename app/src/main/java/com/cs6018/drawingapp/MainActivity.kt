f9917b26c1dee7fee3350924fdbf3c7ff060167d
Jo Song <111762332+songjiuc@users.noreply.github.com>
2023-08-28 15:24:19 -0600

    added a button (#1)

--- app/src/main/java/com/cs6018/drawingapp/MainActivity.kt
+++ app/src/main/java/com/cs6018/drawingapp/MainActivity.kt
@@ -5,16 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.cs6018.drawingapp.ui.theme.DrawingAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,6 +42,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val (buttonClicked, setButtonClicked) = remember { mutableStateOf(false) }
+
     Column(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -46,6 +54,29 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             textAlign = TextAlign.Center,
             modifier = Modifier.fillMaxWidth()
         )
+
+        // Add a button below the text
+        Row(
+            modifier = Modifier.padding(16.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Button(
+                onClick = {
+                          // change the button text when the button is clicked
+                          setButtonClicked(true)
+                } ,
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Text(text = "Click me")
+            }
+        }
+        if(buttonClicked){
+            Text(
+                text = "Button Clicked!",
+                textAlign = TextAlign.Center,
+                modifier=Modifier.fillMaxWidth()
+            )
+        }
     }
 }
 
