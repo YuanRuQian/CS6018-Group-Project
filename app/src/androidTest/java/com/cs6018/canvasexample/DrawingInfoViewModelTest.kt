608ffac7b34063548e0638532ad7a9a48aba689e
Lydia Yuan <lydiayuan99@gmail.com>
2023-10-05 10:33:30 -0600

    add: test view model

--- app/src/androidTest/java/com/cs6018/canvasexample/DrawingInfoViewModelTest.kt
+++ app/src/androidTest/java/com/cs6018/canvasexample/DrawingInfoViewModelTest.kt
@@ -1,12 +1,19 @@
 package com.cs6018.canvasexample
 
 import android.content.Context
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.testing.TestLifecycleOwner
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import com.cs6018.canvasexample.DrawingInfoRepository as Repository
 
@@ -16,17 +23,75 @@ class DrawingInfoViewModelTest {
     private lateinit var db: DrawingInfoDatabase
     private lateinit var repository: Repository
     private lateinit var scope: CoroutineScope
+    private lateinit var viewModel: DrawingInfoViewModel
 
     @Before
     fun setup() {
         // create a database in memory
         val context = ApplicationProvider.getApplicationContext<Context>()
         db = Room.inMemoryDatabaseBuilder(
-            context, DrawingInfoDatabase::class.java).build()
+            context, DrawingInfoDatabase::class.java
+        ).build()
         dao = db.drawingInfoDao()
         scope = CoroutineScope(Dispatchers.IO)
         repository = Repository(scope, dao)
+        viewModel = DrawingInfoViewModel(repository)
     }
 
+    @Test
+    fun testAddDrawingInfo() {
+        val drawingInfoList = generateRandomTestDrawingInfoList(10)
+        for (drawingInfo in drawingInfoList) {
+            viewModel.addDrawingInfo(drawingInfo.drawingTitle, null, null)
+        }
 
+        runBlocking {
+            val lifecycleOwner = TestLifecycleOwner()
+
+            lifecycleOwner.run {
+                withContext(Dispatchers.Main) {
+                    dao.allDrawingInfo().asLiveData().observe(lifecycleOwner) {
+                        viewModel.allDrawingInfo.value?.containsAll(it)
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun testActiveCapturedImage() {
+        val bitmap = generateRandomBitmap(100, 100)
+
+        scope.launch {
+            viewModel.setActiveCapturedImage(bitmap)
+            val fetchedViewModel = viewModel.getActiveCapturedImage().value
+            assert(fetchedViewModel == bitmap)
+        }
+    }
+
+    @Test
+    fun testUpdateThumbnail() {
+        val thumbnail = generateRandomByteArray(100)
+
+        scope.launch{
+            viewModel.setActiveDrawingInfoById(1)
+            viewModel.updateThumbnailForActiveDrawingInfo(thumbnail)
+            val fetchedDrawingInfo = db.drawingInfoDao().fetchDrawingInfoWithId(1).firstOrNull()
+            assert(fetchedDrawingInfo?.thumbnail?.contentEquals(thumbnail) ?: false)
+        }
+    }
+
+    @Test
+    fun testAddDrawingInfoWithRecentCapturedImage() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val bitmap = generateRandomBitmap(100, 100)
+
+        scope.launch {
+            viewModel.setActiveCapturedImage(bitmap)
+            viewModel.setActiveDrawingInfoById(1)
+            val imagePath = viewModel.addDrawingInfoWithRecentCapturedImage(context)
+            assert(imagePath != null)
+            println("Image path: $imagePath")
+        }
+    }
 }
