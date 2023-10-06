a53c52613268f11780d3663318eb6c77b580eb17
Lydia Yuan <lydiayuan99@gmail.com>
2023-10-05 23:15:46 -0600

    refactor: remove observer after single test

--- app/src/androidTest/java/com/cs6018/canvasexample/DatabaseTest.kt
+++ app/src/androidTest/java/com/cs6018/canvasexample/DatabaseTest.kt
@@ -1,13 +1,13 @@
 package com.cs6018.canvasexample
 
 import android.content.Context
+import androidx.lifecycle.Observer
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.testing.TestLifecycleOwner
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import org.junit.After
@@ -22,13 +22,13 @@ import java.util.Date
 class DatabaseTest {
     private lateinit var dao: DrawingInfoDAO
     private lateinit var db: DrawingInfoDatabase
-    private var firstImageAdded = false
 
     @Before
     fun createDb() {
         val context = ApplicationProvider.getApplicationContext<Context>()
         db = Room.inMemoryDatabaseBuilder(
-            context, DrawingInfoDatabase::class.java).build()
+            context, DrawingInfoDatabase::class.java
+        ).build()
         dao = db.drawingInfoDao()
     }
 
@@ -46,13 +46,13 @@ class DatabaseTest {
             lifecycleOwner.run {
                 withContext(Dispatchers.Main) {
                     val allDrawing = dao.allDrawingInfo().asLiveData()
-                    allDrawing.observe(lifecycleOwner) {
-                        if (it.isNotEmpty() && !firstImageAdded) {
-                            Assert.assertEquals("TestImage", dao.fetchDrawingInfoWithId(0).asLiveData().value?.drawingTitle)
+                    allDrawing.observe(lifecycleOwner, object : Observer<List<DrawingInfo>> {
+                        override fun onChanged(value: List<DrawingInfo>) {
+                            Assert.assertEquals("TestImage", value[0].drawingTitle)
+                            allDrawing.removeObserver(this)
                         }
-                    }
+                    })
                     dao.addDrawingInfo(info)
-                    firstImageAdded = true
                 }
             }
         }
@@ -61,20 +61,33 @@ class DatabaseTest {
     @Test
     fun testDeleteADrawing() {
         runBlocking {
-            val deferred = async { dao.deleteAllDrawingInfo() }
             val lifecycleOwner = TestLifecycleOwner()
             val info = DrawingInfo(Date(), Date(), "TestImage", null, null)
             var count = 0
             lifecycleOwner.run {
                 withContext(Dispatchers.Main) {
-                    dao.allDrawingInfo().asLiveData().observe(lifecycleOwner) {
-                        Assert.assertEquals(count, it.size)
-                    }
+                    val allDrawing = dao.allDrawingInfo().asLiveData()
+                    allDrawing
+                        .observe(lifecycleOwner, object : Observer<List<DrawingInfo>> {
+                            override fun onChanged(value: List<DrawingInfo>) {
+                                when (count) {
+                                    1 -> {
+                                        Assert.assertEquals(1, value.size)
+                                        Assert.assertEquals("TestImage", value[0].drawingTitle)
+                                    }
+
+                                    2 -> {
+                                        Assert.assertEquals(0, value.size)
+                                        allDrawing.removeObserver(this)
+                                    }
+                                }
+                            }
+                        })
+
                     dao.addDrawingInfo(info)
                     count += 1
                     dao.deleteDrawingInfoWithId(0)
-                    count -= 1
-                    deferred.await()
+                    count += 2
                 }
             }
         }
