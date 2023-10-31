0eb61d32e0cdc25ee1f9f0362ac7faa6208fb969
Sonia-96 <672365639@qq.com>
2023-10-30 21:31:13 -0600

    add emial and password validation

--- androidApp/app/src/main/java/com/cs6018/canvasexample/ui/components/AuthenticationScreen.kt
+++ androidApp/app/src/main/java/com/cs6018/canvasexample/ui/components/AuthenticationScreen.kt
@@ -3,10 +3,13 @@ package com.cs6018.canvasexample.ui.components
 
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -18,15 +21,17 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
@@ -35,7 +40,6 @@ import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.cs6018.canvasexample.network.ApiViewModel
 import com.google.firebase.auth.FirebaseUser
 
 // TODO: bonus task -> sign in with Google
@@ -47,7 +51,12 @@ fun AuthenticationScreen(
     preloadCurrentUserDrawingHistory: (String) -> Unit,
 ) {
     var email by rememberSaveable { mutableStateOf("") }
+    var isEmailError by remember { mutableStateOf(false) }
+    var emailErrorMessage by remember { mutableStateOf("Invalid email address") }
+
     var password by rememberSaveable { mutableStateOf("") }
+    var isPasswordError by remember { mutableStateOf(false) }
+    var passwordErrorMessage by remember { mutableStateOf("Invalid email address") }
     var passwordHidden by rememberSaveable { mutableStateOf(true) }
 
     val context = LocalContext.current
@@ -63,34 +72,66 @@ fun AuthenticationScreen(
             modifier = Modifier
                 .padding(16.dp)
         )
-        TextField(
-            value = email,
-            singleLine = true,
-            onValueChange = { email = it },
-            label = { Text("Email") },
-            leadingIcon = { Icon(Icons.Filled.Email, contentDescription = "Email") },
-            placeholder = { Text("example@gmail.com") }
-        )
 
-        TextField(
-            value = password,
-            singleLine = true,
-            onValueChange = { password = it },
-            label = { Text("Password") },
-            leadingIcon = { Icon(Icons.Filled.AccountCircle, contentDescription = "Password") },
-            placeholder = { Text("********") },
-            visualTransformation =
-            if (passwordHidden) PasswordVisualTransformation() else VisualTransformation.None,
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-            trailingIcon = {
-                IconButton(onClick = { passwordHidden = !passwordHidden }) {
-                    val visibilityIcon =
-                        if (passwordHidden) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
-                    val description = if (passwordHidden) "Show password" else "Hide password"
-                    Icon(imageVector = visibilityIcon, contentDescription = description)
-                }
+        Column {
+            OutlinedTextField(
+                value = email,
+                singleLine = true,
+                onValueChange = {
+                    email = it
+                },
+                label = { Text("Email") },
+                leadingIcon = { Icon(Icons.Filled.Email, contentDescription = "Email") },
+                placeholder = { Text("example@gmail.com") },
+            )
+
+            if (isEmailError) {
+                Text(
+                    text = emailErrorMessage,
+                    color = Color.Red,
+                    fontSize = 14.sp,
+                    modifier = Modifier
+                        .padding(start = 12.dp, top = 4.dp)
+                )
+            } else {
+                Spacer(modifier = Modifier.height(30.dp))
             }
-        )
+        }
+
+        Column {
+            OutlinedTextField(
+                value = password,
+                singleLine = true,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                leadingIcon = { Icon(Icons.Filled.AccountCircle, contentDescription = "Password") },
+                placeholder = { Text("********") },
+                visualTransformation =
+                if (passwordHidden) PasswordVisualTransformation() else VisualTransformation.None,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                trailingIcon = {
+                    IconButton(onClick = { passwordHidden = !passwordHidden }) {
+                        val visibilityIcon =
+                            if (passwordHidden) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+                        val description = if (passwordHidden) "Show password" else "Hide password"
+                        Icon(imageVector = visibilityIcon, contentDescription = description)
+                    }
+                }
+            )
+
+            if (isPasswordError) {
+                Text(
+                    text = passwordErrorMessage,
+                    color = Color.Red,
+                    fontSize = 14.sp,
+                    modifier = Modifier
+                        .padding(start = 12.dp, top = 4.dp)
+                )
+            } else {
+                Spacer(modifier = Modifier.height(30.dp))
+            }
+        }
+
 
         Row(
             modifier = Modifier.fillMaxWidth(),
@@ -99,46 +140,58 @@ fun AuthenticationScreen(
             // Sign Up Button
             Button(
                 onClick = {
-                    createUserWithEmailAndPassword(email, password, { user ->
-                        Toast.makeText(
-                            context,
-                            "Welcome to Drawing App, ${user?.email}",
-                            Toast.LENGTH_LONG
-                        )
-                            .show()
-                        navigateToDrawingList()
-                    }, {
-                        Toast.makeText(context, "Sign up failed", Toast.LENGTH_LONG).show()
-                    })
+                    isEmailError = !isValidEmail(email)
+                    emailErrorMessage = if (isEmailError) "Invalid email address" else ""
+                    isPasswordError = !isValidPassword(password)
+                    passwordErrorMessage = if (isPasswordError) "Password can't be empty" else ""
+                    if (!isEmailError && !isPasswordError) {
+                        createUserWithEmailAndPassword(email, password, { user ->
+                            Toast.makeText(
+                                context,
+                                "Welcome to Drawing App, ${user?.email}",
+                                Toast.LENGTH_LONG
+                            )
+                                .show()
+                            navigateToDrawingList()
+                        }, {
+                            Toast.makeText(context, "Sign up failed", Toast.LENGTH_LONG).show()
+                        })
+                    }
                 },
                 modifier = Modifier
                     .weight(1f)
-                    .padding(16.dp)
+                    .padding(start = 65.dp, end = 15.dp)
             ) {
                 Text("Sign Up")
             }
 
-            // Sign In Button
+            // Log In Button
             Button(
                 onClick = {
-                    signInWithEmailAndPassword(email, password, { user ->
-                        Toast.makeText(
-                            context,
-                            "Welcome Back, ${user?.email}",
-                            Toast.LENGTH_LONG
-                        )
-                            .show()
-                        preloadCurrentUserDrawingHistory(user?.uid ?: "")
-                        navigateToDrawingList()
-                    }, {
-                        Toast.makeText(context, "Sign In failed", Toast.LENGTH_LONG).show()
-                    })
+                    isEmailError = !isValidEmail(email)
+                    emailErrorMessage = if (isEmailError) "Invalid email address" else ""
+                    isPasswordError = !isValidPassword(password)
+                    passwordErrorMessage = if (isPasswordError) "Password can't be empty" else ""
+                    if (!isEmailError && !isPasswordError) {
+                        signInWithEmailAndPassword(email, password, { user ->
+                            Toast.makeText(
+                                context,
+                                "Welcome Back, ${user?.email}",
+                                Toast.LENGTH_LONG
+                            )
+                                .show()
+                            preloadCurrentUserDrawingHistory(user?.uid ?: "")
+                            navigateToDrawingList()
+                        }, {
+                            Toast.makeText(context, "Log In failed", Toast.LENGTH_LONG).show()
+                        })
+                    }
                 },
                 modifier = Modifier
                     .weight(1f)
-                    .padding(16.dp)
+                    .padding(start = 15.dp, end = 65.dp)
             ) {
-                Text("Sign In")
+                Text("Log In")
             }
         }
 
@@ -151,4 +204,14 @@ fun AuthenticationScreenPreview() {
     MaterialTheme {
         AuthenticationScreen({ _, _, _, _ -> }, { _, _, _, _ -> }, {}, {})
     }
+}
+
+
+fun isValidEmail(email: String): Boolean {
+    val emailRegex = "^[A-Za-z](.*)([@]{1})(.{1,})([.]{1})(.{1,})$"
+    return email.matches(emailRegex.toRegex())
+}
+
+fun isValidPassword(password: String): Boolean {
+    return password != "";
 }
